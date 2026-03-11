34ef6950de3fe03ffb62fb95c367aba8af5020c6
Bipin Singh <bipin_singh@na.honda.com>
2026-03-11 17:59:32 -0400

    v1

---
+++ Controllers/NewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COREWebAPI.Controllers
+{
+    public class NewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
