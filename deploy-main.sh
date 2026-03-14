deac1407f934e0e65cc85a6698f6a25af849299f
Bipin Singh <bipin_singh@na.honda.com>
2026-03-14 10:24:22 -0400

    201

---
+++ deploy-main.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+echo "Deploying application to main..."
+
+# Example build
+npm install
+npm run build
+
+# Example deploy
+scp -r build/* user@main-server:/var/www/app
+
+echo "Deployment to main complete!"
